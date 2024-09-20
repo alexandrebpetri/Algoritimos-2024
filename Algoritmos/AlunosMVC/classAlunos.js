ab4537afec33768eeea99f310413bb70d7d24539
Alexandre Baum Petri <168186976+Xandoka09@users.noreply.github.com>
2024-09-20 09:51:38 -0300

    Add files via upload

---
+++ Algoritmos/AlunosMVC/classAlunos.js
@@ -0,0 +1,10 @@
+class Alunos {
+    constructor(ra, nome, n1, n2, n3, n4) {
+        this.ra = ra;
+        this.nome = nome;
+        this.n1 = n1;
+        this.n2 = n2;
+        this.n3  = n3;
+        this.n4 = n4;
+    }
+}
