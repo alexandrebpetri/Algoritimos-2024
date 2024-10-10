872ae4aff7163f2a90c25429b4bbcd6482e99e60
Alexandre Baum Petri <168186976+Xandoka09@users.noreply.github.com>
2024-10-10 13:39:22 -0300

    Add files via upload

---
+++ Algoritmos/CRUD clubes/CRUD clubes/clubes.js
@@ -0,0 +1,12 @@
+class Clube {
+    constructor(nome, criacao, nacionais, copas, libertadores, mundiais, posicaoNaLista) {
+        this. nome = nome;
+        this.criacao = criacao;
+        this.nacionais = nacionais;
+        this.copas = copas;
+        this.libertadores = libertadores;
+        this.mundiais = mundiais;
+
+        this.posicaoNaLista = posicaoNaLista; //atributo para facilitar a alteração e exclusão
+    }
+}
