04a165a5fb290159316177f121926f43e9f4414b
Alexandre Baum Petri <168186976+Xandoka09@users.noreply.github.com>
2024-10-14 13:50:48 -0300

    Add files via upload

---
+++ Exercícios feitos em sala de aula/AtletasMVC/atleta.js
@@ -0,0 +1,10 @@
+class Atleta {
+    constructor(num, nome, dataNascimento, posicao, altura, peso) {
+        this.num = num;
+        this.nome = nome;
+        this.dataNascimento = dataNascimento;
+        this.posicao = posicao;
+        this.altura = altura;
+        this.peso = peso;
+    }
+}
