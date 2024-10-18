fcf7bf0227a9373b77d494b099e123ab616d6f9b
Xandoka09 <xandoka2009@gmail.com>
2024-10-18 14:20:50 -0300

    bungas

---
+++ Exercícios feitos em sala de aula/AtletasMVC/atleta.js
@@ -0,0 +1,22 @@
+<<<<<<< HEAD
+class Atleta {
+    constructor(num, nome, dataNascimento, posicao, altura, peso) {
+        this.num = num;
+        this.nome = nome;
+        this.dataNascimento = dataNascimento;
+        this.posicao = posicao;
+        this.altura = altura;
+        this.peso = peso;
+    }
+=======
+class Atleta {
+    constructor(num, nome, dataNascimento, posicao, altura, peso) {
+        this.num = num;
+        this.nome = nome;
+        this.dataNascimento = dataNascimento;
+        this.posicao = posicao;
+        this.altura = altura;
+        this.peso = peso;
+    }
+>>>>>>> 78cb197903966e6bee2fd38a81e1ebab1af66cf3
+}
